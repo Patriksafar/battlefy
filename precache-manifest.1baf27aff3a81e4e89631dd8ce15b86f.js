9ab4babdbd7d063a382a19e55a9f0f7a55c7f962
Patriksafar <Patriksafar@users.noreply.github.com>
2020-04-26 15:25:51 +0000

    deploy: 1c3826cfff9b4b52fb010d3119976dda99bf1835

---
+++ precache-manifest.1baf27aff3a81e4e89631dd8ce15b86f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "926e1d67965992b518005f0f25686be7",
+    "url": "/battlefy/index.html"
+  },
+  {
+    "revision": "4cd1b9b0ae519591fb0b",
+    "url": "/battlefy/static/css/main.8d3fe20a.chunk.css"
+  },
+  {
+    "revision": "c374160d628da4af0be4",
+    "url": "/battlefy/static/js/2.8886bc70.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/battlefy/static/js/2.8886bc70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cd1b9b0ae519591fb0b",
+    "url": "/battlefy/static/js/main.d66aafae.chunk.js"
+  },
+  {
+    "revision": "f4f06e506122fa974d2f",
+    "url": "/battlefy/static/js/runtime-main.d85187ac.js"
+  }
+]);
