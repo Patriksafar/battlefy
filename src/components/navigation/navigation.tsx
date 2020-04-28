adbbd997e39abe5ac381d8121cb46c2f1345745f
Patriksafar <safarpatrik@seznam.cz>
2020-04-28 22:44:12 +0200

    fix: invalid paths

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -35,7 +35,7 @@ export const Navigation = () => {
             </Link>
           </MenuItem>
           <MenuItem>
-            <Link to={paths.tournament} color="inherit">
+            <Link to={paths.tournamentDetail} color="inherit">
               Turnaje
             </Link>
           </MenuItem>
